26a3058daccc7d09a9661533a01a2d5164580886
amanda1686 <amanda160986@gmail.com>
2024-05-09 18:50:29 +0200

    feat change button

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import {  Alert, Button, Label,TextInput } from 'flowbite-react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { signInStart, signInSuccess, signInFailure } from '../../Components/Redux/User/userSlice.js';
@@ -44,70 +44,70 @@ const handleSubmit = async (e) =>{
   }
 }
   return (
-    <div className=' min-h-screen mt-40'>
-      <div className=" flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        {/* left */}
-        <div className=" flex-1">
-        <Link to="/" className='  font-bold dark:text-white text-4xl'>
-        <img
-            src={logo}
-            width={250}
-            height={250}
-            className="rounded-full"
-            alt="Logo"
-            />
-        </Link>
-        <p className=' text-sm mt-5'>
-          This is a demo proyect. You can sign in with your email and password or with Google
-          </p>
-        </div>
-        {/* right */}
-        <div className=" flex-1">
-          <form className=' flex flex-col gap-4' onSubmit={handleSubmit}>
-            <div className="">
-              <Label value='Your email'/>
-              <TextInput 
-              type='email'
-              placeholder='name@company.com'
-              id='email' onChange={handleChange}
+
+      <div className='min-h-screen mt-40'>
+        <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+          {/* left */}
+          <div className="flex-1">
+            <Link to="/" className='font-bold dark:text-white text-4xl'>
+              <img
+                src={logo}
+                width={250}
+                height={250}
+                className="rounded-full"
+                alt="Logo"
               />
-            </div>
-            <div className="">
-              <Label value='Your password'/>
-              <TextInput 
-              type='password'
-              placeholder='********'
-              id='password' onChange={handleChange}
-              />
-            </div>
-            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
-              {
-                loading ? (
-                  <>
-                  <Spinner size='sm'/>
-                  <span className=' pl-3'>Loading...</span>                  
-                  </>
-                ) : (
-                  'Sign In'
-                )}
-            </Button>
-            <OAuth/>
-          </form>
-          <div className=" flex gap-3 text-sm mt-5">
-            <span> Don't have an account?</span>
-            <Link to='/signup' className='text-blue-500'>
-              Sign Up
             </Link>
+            <p className='text-sm mt-5'>
+              This is a demo project. You can sign in with your email and password or with Google.
+            </p>
           </div>
-          {
-            errorMessage && (
-              <Alert className=' mt-5'color='failure'>
+          {/* right */}
+          <div className="flex-1">
+            <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+              <div>
+                <Label value='Your email'/>
+                <TextInput 
+                  type='email'
+                  placeholder='name@company.com'
+                  id='email' 
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <Label value='Your password'/>
+                <TextInput 
+                  type='password'
+                  placeholder='********'
+                  id='password' 
+                  onChange={handleChange}
+                />
+              </div>
+              <Button 
+                className='bg-gradient-to-r from-[#AEAF50] to-[#F3C14C] hover:from-[#adaf50bd] hover:to-[#F3C14C] text-white font-bold justify-center rounded transition-colors duration-300 h-8' 
+                type='submit' 
+              >
+                Sign In
+              </Button>
+              <OAuth/>
+            </form>
+            <div className="flex flex-col items-center mt-5">
+              <div className="flex gap-3 text-sm">
+                <span>Don't have an account?</span>
+                <Link to='/signup' className='text-blue-500'>
+                  Sign Up
+                </Link>
+              </div>
+              {errorMessage && (
+                <Alert color='failure' className='mt-5'>
                 {errorMessage}
-              </Alert>
-            )
-          }
+
+                </Alert>
+              )}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    );
+    
 }
