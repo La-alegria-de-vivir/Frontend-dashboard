fef8bcceb5b2d1dec70f2964551e0a3bb66e0549
amanda1686 <amanda160986@gmail.com>
2024-05-10 18:34:24 +0200

    feat:complete dashuser

--- src/Components/DashMenu/DashMenu.jsx
+++ src/Components/DashMenu/DashMenu.jsx
@@ -1,5 +1,4 @@
-import { Modal, Table, Button } from 'flowbite-react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
@@ -10,7 +9,6 @@ export default function DashMenu() {
   const [showMore, setShowMore] = useState(true);
   const [showModal, setShowModal] = useState(false);
   const [menuIdToDelete, setMenuIdToDelete] = useState('');
-
 
   useEffect(() => {
     const fetchMenu = async () => {
@@ -74,108 +72,97 @@ export default function DashMenu() {
 
   return (
     <div className=' mt-40 table-auto overflow-x-scroll md:mx-auto p-3 scrollbar'>
- {currentUser.isAdmin && userMenu.length > 0 ? (
-            <>
-    <Table hoverable className='shadow-md'>
-      <Table.Head>
-        <Table.HeadCell>Date updated</Table.HeadCell>
-        <Table.HeadCell>Menu image</Table.HeadCell>
-        <Table.HeadCell>Menu title</Table.HeadCell>
-        <Table.HeadCell>Precio</Table.HeadCell>
-        <Table.HeadCell>Borrar</Table.HeadCell>
-        
-        <Table.HeadCell>
-          <span>Editar</span>
-        </Table.HeadCell>
-      </Table.Head>
-      {userMenu.menu.map((menu) => (
-        <Table.Body className='divide-y'>
-          <Table.Row className=''>
-            <Table.Cell>
-              {new Date(menu.updatedAt).toLocaleDateString()}
-            </Table.Cell>
-            <Table.Cell>
-              <Link to={`/menu/${menu.slug}`}>
-                <img
-                  src={menu.image}
-                  alt={menu.title}
-                  className='w-20 h-10 object-cover bg-black-500'
-                />
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              <Link
-                className='font-medium text-gray-900 dark:text-white'
-                to={`/menu/${menu.slug}`}
-              >
-                {menu.title}
-              </Link>
-            </Table.Cell>
-            <Table.Cell>{menu.category}</Table.Cell>
+      {currentUser.isAdmin && userMenu.length > 0 ? (
+        <>
+          <table className='shadow-md'>
+            <thead>
+              <tr>
+                <th>Date updated</th>
+                <th>Menu image</th>
+                <th>Menu title</th>
+                <th>Precio</th>
+                <th>Borrar</th>
+                <th>Editar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {userMenu.map((menu) => (
+                <tr key={menu._id}>
+                  <td>{new Date(menu.updatedAt).toLocaleDateString()}</td>
+                  <td>
+                    <Link to={`/menu/${menu.slug}`}>
+                      <img
+                        src={menu.image}
+                        alt={menu.title}
+                        className='w-20 h-10 object-cover bg-black-500'
+                      />
+                    </Link>
+                  </td>
+                  <td>
+                    <Link
+                      className='font-medium text-gray-900 dark:text-white'
+                      to={`/menu/${menu.slug}`}
+                    >
+                      {menu.title}
+                    </Link>
+                  </td>
+                  <td>{menu.category}</td>
+                  <td>
+                    <span
+                      onClick={() => {
+                        setShowModal(true);
+                        setMenuIdToDelete(menu._id);
+                      }}
+                      className='font-medium text-red-500 hover:underline cursor-pointer'
+                    >
+                      Borrar
+                    </span>
+                  </td>
+                  <td>
+                    <Link
+                      className='text-teal-500 hover:underline'
+                      to={`/update-menu/${menu._id}`}
+                    >
+                      Editar
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Mostrar Más
+            </button>
+          )}
+        </>
+      ) : (
+        <p>No tienes ningún menú!</p>
+      )}
 
-            <Table.Cell>
-              <span
-                onClick={() => {
-                  setShowModal(true);
-                  setMenuIdToDelete(menu._id);
-                }}
-                className='font-medium text-red-500 hover:underline cursor-pointer'
-              >
-                Borrar
-              </span>
-            </Table.Cell>
-            
-            <Table.Cell>
-              <Link
-                className='text-teal-500 hover:underline'
-                to={`/update-menu/${post._id}`}
-              >
-                <span>Editar</span>
-              </Link>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      ))}
-    </Table>
-    {showMore && (
-      <button
-        onClick={handleShowMore}
-        className='w-full text-teal-500 self-center text-sm py-7'
-      >
-       Mostrar Más
-      </button>
-    )}
-  </>
-) : (
-  
-  <p>No tienes ningún menú!</p>
- 
-)}
-
-      <Modal
-        show={showModal}
-        onClose={() => setShowModal(false)}
-        popup
-        size='md'
-      >
-        <Modal.Header />
-        <Modal.Body>
-          <div className='text-center'>
-            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
-            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
-              Are you sure you want to delete this menu?
-            </h3>
-            <div className='flex justify-center gap-4'>
-              <Button color='failure' onClick={handleDeleteMenu}>
-                Yes, I'm sure
-              </Button>
-              <Button color='gray' onClick={() => setShowModal(false)}>
-                No, cancel
-              </Button>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <div className='text-center'>
+              <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+              <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+                Are you sure you want to delete this menu?
+              </h3>
+              <div className='flex justify-center gap-4'>
+                <button className='btn btn-danger' onClick={handleDeleteMenu}>
+                  Yes, I'm sure
+                </button>
+                <button className='btn btn-secondary' onClick={() => setShowModal(false)}>
+                  No, cancel
+                </button>
+              </div>
             </div>
           </div>
-        </Modal.Body>
-      </Modal>
+        </div>
+      )}
     </div>
   );
 }
