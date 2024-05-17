b1987729adcd0a4f76fbe9abc9d5614b47ad25dc
amanda1686 <amanda160986@gmail.com>
2024-05-17 14:05:38 +0200

    feat:complete navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,10 +6,8 @@ function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
   
-  // Lógica para determinar si el usuario está autenticado...
   const isAuthenticated = true; // Reemplaza esto con tu lógica real
 
-  // Función para manejar el clic en el logo
   const handleLogoClick = () => {
     if (isAuthenticated) {
       navigate('/dashboard?tab=profile');
@@ -18,7 +16,6 @@ function Navbar() {
     }
   };
 
-  // Función para manejar el clic en el enlace de "Inicio"
   const handleInicioClick = () => {
     if (isAuthenticated) {
       navigate('/dashboard?tab=profile');
@@ -29,23 +26,21 @@ function Navbar() {
 
   return (
     <div className="fixed top-0 w-full z-50 bg-white text-black">
-      <section className="relative mx-auto">
-        {/* Navbar */}
-        <nav className="flex justify-between">
-          <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-            {/* Logo */}
+      <section className="container mx-auto px-4">
+        <nav className="flex justify-between items-center py-6">
+          <div className="flex items-center">
             <a className="text-3xl font-bold font-heading" onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
-  <img
-    src={logo}
-    style={{ width: '50px', height: 'auto' }} // Establece el ancho deseado y deja que la altura se ajuste automáticamente
-    className=""
-    alt="Logo"
-  />
-</a>
-
-            {/* Menu Hamburguesa */}
+              <img
+                src={logo}
+                style={{ width: '50px', height: 'auto' }}
+                className=""
+                alt="Logo"
+              />
+            </a>
+          </div>
+          <div className="flex items-center">
             <button
-              className="md:hidden text-xl focus:outline-none"
+              className="md:hidden text-xl focus:outline-none ml-auto"
               onClick={() => setMenuOpen(!menuOpen)}
             >
               <svg
@@ -72,22 +67,22 @@ function Navbar() {
                 )}
               </svg>
             </button>
-            {/* Nav Links */}
-            <ul className={`${menuOpen ? 'flex flex-col w-screen items-center space-y-4' : 'hidden'} md:hidden`}>
-              <li><a className="hover:text-[#BBBC4E] cursor-pointer" onClick={handleInicioClick}>Inicio</a></li>
-              <li><Link className="hover:text-[#BBBC4E]" to="/alergenos">Alérgenos</Link></li>
-              <li><Link className="hover:text-[#BBBC4E]" to="/reservations">Reservaa</Link></li>
-              <li><Link className="hover:text-[#BBBC4E]" to="/create-menu">Menú</Link></li>
-              <li><Link className="hover:text-[#BBBC4E]" to="#">Contacto</Link></li>
-            </ul>
-            <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-              <li><a className="hover:text-[#BBBC4E] cursor-pointer" onClick={handleInicioClick}>Inicio</a></li>
-              <li><Link className="hover:text-[#BBBC4E]" to="/alergenos">Alérgenos</Link></li>
-              <li><Link className="hover:text-[#BBBC4E]" to="/reservations">Reservas</Link></li>
-              <li><Link className="hover:text-[#BBBC4E]" to="/create-menu">Menú</Link></li>
-              <li><Link className="hover:text-[#BBBC4E]" to="#">Contacto</Link></li>
-            </ul>
           </div>
+          <ul className={`${menuOpen ? 'flex flex-col w-full items-center space-y-4' : 'hidden'} md:hidden`}>
+            <li><a className="hover:text-[#BBBC4E] cursor-pointer" onClick={handleInicioClick}>Inicio</a></li>
+            <li><Link className="hover:text-[#BBBC4E]" to="/alergenos">Alérgenos</Link></li>
+            <li><Link className="hover:text-[#BBBC4E]" to="/reservations">Reservas</Link></li>
+            <li><Link className="hover:text-[#BBBC4E]" to="/create-menu">Menú</Link></li>
+            <li><Link className="hover:text-[#BBBC4E]" to="#">Contacto</Link></li>
+          </ul>
+          <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+            <li><a className="hover:text-[#BBBC4E] cursor-pointer" onClick={handleInicioClick}>Inicio</a></li>
+            <li><a className="hover:text-[#BBBC4E] cursor-pointer" onClick={handleInicioClick}>Inicio</a></li>
+            <li><Link className="hover:text-[#BBBC4E]" to="/alergenos">Alérgenos</Link></li>
+            <li><Link className="hover:text-[#BBBC4E]" to="/reservations">Reservas</Link></li>
+            <li><Link className="hover:text-[#BBBC4E]" to="/create-menu">Menú</Link></li>
+            <li><Link className="hover:text-[#BBBC4E]" to="#">Contacto</Link></li>
+          </ul>
         </nav>
       </section>
     </div>
